424bcb670d3fd2a9d4d846a45505c33e37e61e7e
Elizaveta <khmelkh@yandex.ru>
2023-10-31 11:58:33 +0300

    Initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldTestTheBody(){
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Ночь, улица, фонарь, аптека;") // отправляемые данные
+          // Выполняемые действия
+                .when()
+                .post("/post")
+          // Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("Ночь, улица, фонарь, аптека;"))
+        ;
+    }
+}
