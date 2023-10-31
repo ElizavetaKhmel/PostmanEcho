ba38dbf5072c00635e3ee0e87e3920945b5256f8
Elizaveta <khmelkh@yandex.ru>
2023-10-31 12:29:48 +0300

    Edit test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
           // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("Ночь, улица, фонарь, аптека, бессмысленный и тусклый свет;"))
+                .body("data",equalTo("Ночь, улица, фонарь, аптека;"))
         ;
     }
 }
